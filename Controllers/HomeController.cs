d4bec99b46f399a5db0f43606a9a2e50473c217a
rebeccaleifer <rebecca.leifer15@gmail.com>
2022-02-02 10:12:54 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TaskContext tContext { get; set; }
+
+        public HomeController(TaskContext someName)
+        {
+            tContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EnterTasks()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult EnterTasks()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult ListTasks()
+        {
+            return View();
+        }
+
+        //[HttpGet]
+        //public IActionResult Edit()
+        //{
+        //    return View("ListTasks");
+        //}
+
+        //[HttpPost]
+        //public IActionResult Edit()
+        //{
+        //    tContext.Update(blah);
+        //    tContext.SaveChanges();
+
+        //    return RedirectToAction("ListTasks");
+        //}
+
+        //[HttpGet]
+        //public IActionResult Delete()
+        //{
+
+        //    return View("EnterTasks");
+        //}
+
+        //[HttpPost]
+        //public IActionResult Delete()
+        //{
+        //    tContext.Responses.Remove();
+        //    tContext.SaveChanges();
+
+        //    return RedirectToAction("ListTasks");
+        //}
+    }
+}
