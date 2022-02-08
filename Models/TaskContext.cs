996c0cc64a7c906eb6cd6bc15231756e0b878bb2
dustin-nielsen <dustinultimate@gmail.com>
2022-02-08 09:33:00 -0700

    Models/Database/Setup - Dustin

--- Models/TaskContext.cs
+++ Models/TaskContext.cs
@@ -1,11 +1,61 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mission6.Models
 {
-    public class TaskContext
+    public class TaskContext : DbContext
     {
+        //Constructor
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+            //Leave blank for now
+        }
+
+        public DbSet<TaskResponse> Responses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Home" },
+                new Category { CategoryId = 2, CategoryName = "School" },
+                new Category { CategoryId = 3, CategoryName = "Work" },
+                new Category { CategoryId = 4, CategoryName = "Church" }
+            );
+
+            mb.Entity<TaskResponse>().HasData(
+
+                new TaskResponse
+                {
+                    TaskId = 1,
+                    CategoryId = 4,
+                    TaskDescription = "Wash the car.",
+                    DueDate = new DateTime(2008, 3, 1),
+                    Quadrant = "1",
+                    Completed = true
+                }, 
+                new TaskResponse
+                {
+                    TaskId = 2,
+                    CategoryId = 1,
+                    TaskDescription = "Sleep.",
+                    DueDate = new DateTime(2018, 1, 3),
+                    Quadrant = "4",
+                    Completed = true
+                },
+                new TaskResponse
+                {
+                    TaskId = 3,
+                    CategoryId = 3,
+                    TaskDescription = "Ask out Kate.",
+                    DueDate = new DateTime(2022, 3, 1),
+                    Quadrant = "3",
+                    Completed = false
+                }
+            );
+        }
     }
 }
---
+++ Models/TaskResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class TaskResponse
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+
+        [Required(ErrorMessage = "Task Description is required")]
+        public string TaskDescription { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        [Required(ErrorMessage = "Quadrant is required")]
+        public string Quadrant { get; set; }
+
+        public bool Completed { get; set; }
+
+
+        //Build Foreign Key Relationship
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
